e68528a4f884343dab641a6da10d18b8b0136c90
thenameispandit <divyanshushukla1947@gmail.com>
2025-09-12 12:42:37 +0530

    Time: 0 ms (100.00%) | Memory: 14.7 MB (60.56%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
@@ -4,15 +4,17 @@ public:
 
         int n = s.length();
         vector<int> freq(26,0);
-        queue<int> q;
+        // queue<int> q;
+
+        for(char ch: s){
+            freq[ch-'a']++;
+        }
 
         for(int i=0; i<n; i++){
-            freq[s[i]-'a']++;
-            q.push(i);
-            while(!q.empty() && freq[s[q.front()]-'a']>1){
-                q.pop();
+            if(freq[s[i]-'a']==1){
+                return i;
             }
         }
-        return q.empty() ? -1 : q.front();
+        return -1;
     }
 };
