1ce7266fe5608d301cc8831cf86786dab9ba2079
thenameispandit <divyanshushukla1947@gmail.com>
2025-09-18 10:56:53 +0530

    Time: 11 ms (66.30%) | Memory: 17.3 MB (67.14%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,41 +9,27 @@
  */
 class Solution {
 public:
-    bool findPath(TreeNode* root, TreeNode* p, vector<TreeNode*>&path){
-
-        if(!root){
-            return false;
-        }
-
-        path.push_back(root);
-        if(root == p){
-            return true;
-        }
-
-        bool isLeft = findPath(root->left,p,path);
-        bool isRight = findPath(root->right,p,path);
-
-        if(isLeft || isRight){
-            return true;
-        }
-
-        path.pop_back(); // pop b/c left and right both don't have the node
-        return false;
-    }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        vector<TreeNode*> path1;
-        vector<TreeNode*> path2;
 
-        findPath(root,p,path1);
-        findPath(root,q,path2);
-        TreeNode* lca = nullptr;
-        int minPath = min(path1.size() , path2.size());
-
-        for(int i=0; i<minPath; i++){
-            if(path1[i]==path2[i]){
-                lca = path1[i];
-            }
+        if(!root || root == p || root == q){
+            return root;
         }
-        return lca;
+        TreeNode* leftLCA = lowestCommonAncestor(root->left, p, q);
+        TreeNode* rightLCA = lowestCommonAncestor(root->right, p, q);
+
+        if(leftLCA && rightLCA){
+            return root;
+        }
+
+        if(leftLCA && !rightLCA){
+            return leftLCA;
+        }
+
+        if(!leftLCA && rightLCA){
+            return rightLCA;
+        }
+
+        return nullptr;        
+
     }
 };
