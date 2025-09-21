ecab308c47c43b220c88e2643e8f06b533e4be25
thenameispandit <divyanshushukla1947@gmail.com>
2025-09-21 11:26:59 +0530

    Time: 27 ms (33.62%) | Memory: 44.1 MB (55.94%) - LeetSync

--- 652-find-duplicate-subtrees/find-duplicate-subtrees.cpp
+++ 652-find-duplicate-subtrees/find-duplicate-subtrees.cpp
@@ -13,20 +13,21 @@ class Solution {
 public:
     string helper(TreeNode* root, map<string,int> &subTrees, vector<TreeNode*> &ans){
         if(!root){
-            return "#";
+            return "N";
         }
 
-        string left = helper(root->left,subTrees,ans);
-        string right = helper(root->right, subTrees,ans);
-        string currNode = to_string(root->val);
+        string leftPath = helper(root->left, subTrees, ans);
+        string rightPath = helper(root->right, subTrees, ans);
 
-        string path = currNode+","+left+","+right;
-        if(subTrees[path]==1){
+        string fullPath = to_string(root->val) + "," + leftPath + "," + rightPath;
+
+        subTrees[fullPath]++;
+        if(subTrees[fullPath]==2){
             ans.push_back(root);
         }
-        subTrees[path]++;
 
-        return path;
+        return fullPath;
+
     }
     vector<TreeNode*> findDuplicateSubtrees(TreeNode* root) {
         map<string,int> subTrees;
