931b4f137f4275a82647690d851b61a1523fde17
divyanshu-dev <divyanshushukla1947@gmail.com>
2026-02-05 12:28:43 +0530

    Time: 1887 ms (5.00%) | Memory: 70.8 MB (92.79%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,39 +1,55 @@
 class Solution {
 public:
-    void maxHeapify(vector<int>& nums, int i, int n){
-        int l = 2*i + 1;
-        int r = 2*i + 2;
-        int maxI = i;
+    void heapify(int idx,vector<int> &vec,int n)
+{
+    int largest = idx;
+    int l = 2 * idx + 1;
+    int r = 2 * idx + 2;
 
-        if(l<n && nums[l]>nums[maxI]){
-            maxI = l;
-        }
-
-        if(r<n && nums[r]>nums[maxI]){
-            maxI = r;
-        }
-
-        if(maxI != i){
-            swap(nums[maxI],nums[i]);
-            maxHeapify(nums,maxI,n);
-        }
+    if (l < n && vec[l] > vec[largest])
+    {
+        largest = l;
+    }
+    if (r < n && vec[r] > vec[largest])
+    {
+        largest = r;
     }
 
-    void heapSort(vector<int>& nums){
-        int n = nums.size();
-        // Creating HEap
-        for(int i=n/2-1; i>=0; i--){
-            maxHeapify(nums,i,n);
-        }
+    if (largest != idx)
+    {
+        swap(vec[largest], vec[idx]);
+    }
+    else{
+        return;
+    }
+    heapify(largest,vec,n);
+}
 
-        //Fixing Positions
-        for(int i=n-1; i>=0; i--){
-            swap(nums[0],nums[i]);
-            maxHeapify(nums,0,i);
-        }
+void fixHeap(vector<int> &vec,int n){
+    cout<<"Before heap is fixed"<<endl;
+    for(int i=0; i<vec.size(); i++){
+        cout<<vec[i]<<" ";
     }
-    vector<int> sortArray(vector<int>& nums) {
-        heapSort(nums);
-        return nums;
+    cout<<endl;
+    for(int i=n-1; i>=0; i--){
+        swap(vec[0],vec[i]);
+        heapify(0,vec,i);
     }
+    cout<<"After heap is fixed"<<endl;
+    for(int i=0; i<vec.size(); i++){
+        cout<<vec[i]<<" ";
+    }
+    cout<<endl;
+}
+
+vector<int> sortArray(vector<int> &vec)
+{
+    int n = vec.size();
+    for (int i = n / 2; i >= 0; i--)
+    {
+        heapify(i,vec,n);
+    }
+    fixHeap(vec,n);
+    return vec;
+}
 };
