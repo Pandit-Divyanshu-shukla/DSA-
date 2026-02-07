153f3a04589a184862c2f344ece31e440e191c4b
divyanshu-dev <divyanshushukla1947@gmail.com>
2026-02-07 14:21:22 +0530

    Time: 71 ms (13.06%) | Memory: 153.4 MB (11.53%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -1,31 +1,27 @@
+#define pii pair<int,int>
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        deque<int> dq;
+        priority_queue<pii> pq;
         vector<int> max_wndw;
         int n = nums.size();
 
         for(int i=0; i<k; i++){
-            while(!dq.empty() && nums[dq.back()]<nums[i]){
-                dq.pop_back();
-            }
-            dq.push_back(i);
+            pq.push({nums[i],i});
         }
 
-        max_wndw.push_back(nums[dq.front()]);
+        max_wndw.push_back(pq.top().first);
 
         for(int i=k; i<n; i++){
 
             int s_idx = i-k+1;
-            while(!dq.empty() && dq.front()<s_idx){
-                dq.pop_front();
+            while(!pq.empty() && pq.top().second<s_idx){
+                pq.pop();
             }
 
-            while(!dq.empty() && nums[dq.back()]<nums[i]){
-                dq.pop_back();
-            }
-            dq.push_back(i);
-            max_wndw.push_back(nums[dq.front()]);
+            
+            pq.push({nums[i],i});
+            max_wndw.push_back(pq.top().first);
         }
 
         return max_wndw;
