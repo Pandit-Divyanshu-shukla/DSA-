791e222a9ae6df08548488def4205d964615d9e1
divyanshu-dev <divyanshushukla1947@gmail.com>
2026-02-14 12:37:25 +0530

    Time: 0 ms (100.00%) | Memory: 28.5 MB (7.82%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -2,22 +2,19 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         unordered_map<int,int> mp;
+        int ans;
         for(int i: nums){
             if(!mp.count(i)){
                 mp[i] = 1;
             }else{
                 mp[i] = mp[i]+1;
             }
-            
         }
 
-        int ans;
-        for(pair<int,int> p: mp){
-            cout<<p.first<<" "<<p.second<<endl;
-        }
+        
+
         for(pair<int,int> p: mp){
             if(p.second>=ceil(nums.size()/2.0)){
-                cout<<ceil(nums.size()/2)<<endl;
                 ans = p.first;
                 break;
             }
