01fa74e9dbe64f5e21a9bc9a003394224286c614
divyanshu-dev <divyanshushukla1947@gmail.com>
2026-02-15 11:59:15 +0530

    Time: 0 ms (100.00%) | Memory: 23.5 MB (96.37%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,24 +1,49 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        unordered_map <int,int> mp;
-        int limit = nums.size() / 3;
-        cout<<limit<<endl;
+        int cand1 = -78;
+        int cand2 = -89;
+
+        int cnt1 = 0;
+        int cnt2 = 0;
+        
         vector<int> ans;
-        for(int i: nums){
-            if(!mp.count(i)){
-                mp[i] = 1;
-            }else{
-                mp[i]++;
+
+        for(int x: nums){
+            if(cand1==x){
+                cnt1++;
             }
-            
+            else if(cand2==x){
+                cnt2++;
+            }
+            else if(cnt1==0){
+                cand1 = x;
+                cnt1 = 1;
+            }
+            else if(cnt2==0){
+                cand2 = x;
+                cnt2 = 1;
+            }
+            else{
+                cnt1--;
+                cnt2--;
+            }
         }
 
-        for(pair<int,int>p: mp){
-            if(p.second>limit){
-                ans.push_back(p.first);
-            }
+        cnt1 = 0;
+        cnt2 = 0;
+        for(int i=0; i<nums.size(); i++){
+            if(cand1==nums[i])cnt1++;
+            else if(cand2==nums[i])cnt2++;
         }
+
+        if(cnt1>nums.size()/3){
+            ans.push_back(cand1);
+        }
+        if(cnt2>nums.size()/3){
+            ans.push_back(cand2);
+        }
+
         return ans;
     }
 };
