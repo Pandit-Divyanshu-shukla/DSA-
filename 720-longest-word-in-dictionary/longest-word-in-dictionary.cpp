e189cd3cd1f126238fc93e980b473dc4f04b0fc8
divyanshu-dev <divyanshushukla1947@gmail.com>
2026-03-02 10:00:02 +0530

    Time: 19 ms (69.98%) | Memory: 22.6 MB (84.22%) - LeetSync

--- 720-longest-word-in-dictionary/longest-word-in-dictionary.cpp
+++ 720-longest-word-in-dictionary/longest-word-in-dictionary.cpp
@@ -1,71 +1,17 @@
-class Node{
-public:
-    bool EOW;
-    unordered_map <char, Node*> children;
-    Node(){
-        EOW = false;
-    }
-};
-class Trie{
-    
-public:
-    Node* root;
-
-    Trie(){
-        root = new Node();
-    }
-    
-    void insert(string word){
-        Node* temp = root;
-        for(int i=0; i<word.size(); i++){
-            if(temp->children.count(word[i])==0){
-                temp->children[word[i]] = new Node();
-            }
-            temp = temp->children[word[i]];
-        }
-        temp->EOW = true;
-    }
-    
-    bool search(string word){
-        Node* temp = root;
-        for(int i=0; i<word.size(); i++){
-            if(temp->children.count(word[i])==0){
-                return false;
-            }
-            temp = temp->children[word[i]];
-        }
-        return temp->EOW;
-    }
-    
-    void solve(Node* root, string &ans, string &temp){
-        for(auto &it: root->children){
-            if(it.second->EOW){
-                temp += it.first;
-                if(temp.size()>ans.size()){
-                    ans = temp;
-                }else if(temp.size()==ans.size()){
-                    ans = temp<ans ? temp : ans;
-                }
-                solve(it.second,ans,temp);
-                temp.pop_back();
-            }
-        }
-    }
-        
-    
-};
 class Solution {
   public:
     string longestWord(vector<string> &words) {
-        // code here
-        Trie trie;
-        for(string &st: words){
-            trie.insert(st);
-        }
-        
+        unordered_set<string> present;
+        sort(words.begin(),words.end());
         string ans = "";
-        string temp = "";
-        trie.solve(trie.root,ans,temp);
+        for(string &st: words){
+            if(st.size()==1 || present.count(st.substr(0,st.size()-1))){
+                present.insert(st);
+                if(st.size()>ans.size()){
+                    ans = st;
+                }
+            }
+        }
         
         return ans;
         
