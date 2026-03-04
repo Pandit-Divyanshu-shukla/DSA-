3c06ff0c4cbfe3a393fa3ceb6f238b098a937146
divyanshu-dev <divyanshushukla1947@gmail.com>
2026-03-04 15:36:36 +0530

    Time: 382 ms (10.00%) | Memory: 156 MB (58.43%) - LeetSync

--- 2671-shortest-cycle-in-a-graph/shortest-cycle-in-a-graph.cpp
+++ 2671-shortest-cycle-in-a-graph/shortest-cycle-in-a-graph.cpp
@@ -1,13 +1,7 @@
+#define pii pair<int,int>
 class Solution {
 public:
-    void bfs(int src, vector<bool>& visited, vector<int>&parent, vector<vector<int>> &adj, int &minLen,vector<int> &depth){
-
-        
-    }
     int findShortestCycle(int n, vector<vector<int>>& edges) {
-
-        
-
         vector<vector<int>> adj(n);
 
         for(auto &e: edges){
@@ -18,25 +12,29 @@ public:
         int minLen = INT_MAX;
         
         for(int src=0; src<n; src++){
+
             vector<bool>visited(n,false);
             vector<int> depth(n,0);
-            vector<int> parent(n,-1);
+            // vector<int> parent(n,-1);
+
             if(!visited[src]){
-                queue<int> q;
-                q.push(src);
+                queue<pii> q;
+                q.push({src,-1});
                 visited[src] = true;
                 
                 while(!q.empty()){
 
-                    int node = q.front(); q.pop();
+                    int node = q.front().first;
+                    int par = q.front().second;
+
+                    q.pop();
                     for(int v: adj[node]){
 
                         if(!visited[v]){
                             depth[v] = depth[node] + 1;
-                            q.push(v);
+                            q.push({v,node});
                             visited[v] = true;
-                            parent[v] = node;
-                        }else if(v!=parent[node]){
+                        }else if(v!=par){
                             // cycle
                             int len = depth[node]+depth[v]+1;  // both distance + 1(off edge)
                             minLen = min(len,minLen);
