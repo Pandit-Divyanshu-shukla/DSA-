095c96c79d2d0474a30126bd213e91cbb96f4e1e
divyanshu-dev <divyanshushukla1947@gmail.com>
2026-03-04 16:47:46 +0530

    Time: 178 ms (29.23%) | Memory: 165.8 MB (22.32%) - LeetSync

---
+++ 2439-longest-cycle-in-a-graph/longest-cycle-in-a-graph.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+
+    void dfs(int src, vector<bool> &visited,vector<int> &depth, vector<vector<int>> &adj, vector<bool> &recP, int &maxLen){
+        visited[src] = true;
+        recP[src] = true;
+        for(int neighbour: adj[src]){
+            if(!visited[neighbour]){
+                depth[neighbour] = depth[src] + 1;
+                dfs(neighbour,visited,depth,adj,recP,maxLen);
+            }
+            else if(recP[neighbour] && depth[src]>depth[neighbour]){
+                //cycle
+                maxLen = max(maxLen,depth[src]-depth[neighbour]+1);
+            }
+        }
+        recP[src] = false;
+    }
+    int longestCycle(vector<int>& edges) {
+
+        int n = edges.size();
+
+        vector<int> depth(n,0);
+        vector<bool> visited(n,false);
+        vector<bool> recP(n,false);
+
+        vector<vector<int>> adj(edges.size());
+
+        int maxLen = INT_MIN;
+        for(int e=0; e<edges.size(); e++){
+            if(edges[e] !=-1) adj[e].push_back(edges[e]);
+        }
+        for(int i=0; i<edges.size(); i++){
+            if(!visited[i]){
+                depth[i]=0;
+                dfs(i,visited,depth,adj,recP,maxLen);
+            }
+        }
+        return maxLen==INT_MIN ? -1 : maxLen;
+    }
+};
