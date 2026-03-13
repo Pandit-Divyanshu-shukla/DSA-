02834fb3727f099855cf28e532a32392268a81e5
divyanshu-dev <divyanshushukla1947@gmail.com>
2026-03-13 20:25:05 +0530

    Time: 2 ms (57.48%) | Memory: 28.4 MB (49.81%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.cpp
+++ 695-max-area-of-island/max-area-of-island.cpp
@@ -1,35 +1,35 @@
 class Solution {
 public:
     void dfs(vector<vector<int>>& grid, int i, int j, vector<vector<bool>> &visited, int &currCmpArea){
-        // Checking Range 
-        if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size() || visited[i][j] || grid[i][j]==0) return;
+
+        if(i<0 || i>=grid.size() || j<0 || j>=grid[0].size() || grid[i][j]==0 || visited[i][j]) return;
+
+        visited[i][j]=1;
         ++currCmpArea;
 
-        visited[i][j] = true;
-
         dfs(grid,i+1,j,visited,currCmpArea);
-        dfs(grid,i,j-1,visited,currCmpArea);
         dfs(grid,i-1,j,visited,currCmpArea);
         dfs(grid,i,j+1,visited,currCmpArea);
+        dfs(grid,i,j-1,visited,currCmpArea);
+
     }
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        // if(grid.size()==0){ return 0;}
-
         int m = grid.size();
         int n = grid[0].size();
-
+        
         int maxArea = 0;
 
-        vector<vector<bool>> visited(m,vector<bool>(n,false));
+        vector<vector<bool>> visited(m,vector<bool>(n,0)) ;
+
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
-                if(!visited[i][j] && grid[i][j]==1){
-                    int currCmpArea = 0;
-                    // maxArea++;
-                    dfs(grid,i,j,visited,currCmpArea);
-                    maxArea = max(currCmpArea, maxArea);
+                int curr=0;
+                if(grid[i][j]==1){
+                    dfs(grid,i,j,visited,curr);
+                    maxArea = max(curr,maxArea);
                 }
             }
+            
         }
 
         return maxArea;
