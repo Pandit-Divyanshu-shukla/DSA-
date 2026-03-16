488c8f81ee53025be9d226fa3c6a0ad175ac67df
divyanshu-dev <divyanshushukla1947@gmail.com>
2026-03-16 23:08:25 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (49.49%) - LeetSync

--- 389-find-the-difference/find-the-difference.cpp
+++ 389-find-the-difference/find-the-difference.cpp
@@ -1,19 +1,11 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        vector<int> count(26,0);
-        char ans;
-        for(char ch: t){
-            count[ch-'a']++;
-        }
-        for(char ch: s){
-            count[ch-'a']--;
-        }
-        for(int i=0; i<26; i++){
-            if(count[i]==1){
-                ans = 'a'+i;
-            }
-        }
+        char ans = 0;
+
+        for(char ch: s) ans^=ch;
+        for(char ch: t) ans^=ch;
+
         return ans;
     }
 };
