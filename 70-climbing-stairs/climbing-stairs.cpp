cbfa194f3c6c1a9a021732ec642b9c557d161455
divyanshu-dev <divyanshushukla1947@gmail.com>
2026-03-23 12:36:32 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (17.26%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,13 +1,20 @@
 class Solution {
 public:
-    int ways(int n, vector<int> &dp){
+    // int ways(int n, vector<int> &dp){
+    //     if(n==0 || n==1) return 1;
+
+    //     if(dp[n]!=-1) return dp[n];
+    //     return dp[n] = ways(n-1,dp) + ways(n-2,dp);
+    // }
+    int climbStairs(int n) {
         if(n==0 || n==1) return 1;
 
-        if(dp[n]!=-1) return dp[n];
-        return dp[n] = ways(n-1,dp) + ways(n-2,dp);
-    }
-    int climbStairs(int n) {
-        vector<int> dp(n+1,-1);
-        return ways(n,dp);
+        vector<int> ways(n+1,-1);
+        ways[0] = ways[1] = 1;
+
+        for(int i=2; i<=n; i++){
+            ways[i] = ways[i-1] + ways[i-2];
+        }
+        return ways[n];
     }
 };
