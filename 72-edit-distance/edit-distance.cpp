a77a64e65d9417201432241452db33d387f6c3b5
divyanshu-dev <divyanshushukla1947@gmail.com>
2026-03-31 11:01:41 +0530

    Time: 6 ms (69.66%) | Memory: 13.4 MB (29.61%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -23,8 +23,28 @@ public:
         int l1 = word1.size();
         int l2 = word2.size();
 
-        vector<vector<int>> dp(l1+1,vector<int>(l2+1,-1));
+        vector<vector<int>> dp(l1+1,vector<int>(l2+1,0));
 
-        return solve(word1,word2,l1,l2,dp);
+        for(int i=0; i<=l1; i++){
+            dp[i][0] = i;
+        }
+        for(int j=0; j<=l2; j++){
+            dp[0][j] = j;
+        }
+
+        for(int i=1; i<=l1; i++){
+            for(int j=1; j<=l2; j++){
+                if(word1[i-1]==word2[j-1]){
+                    dp[i][j] = dp[i-1][j-1];
+                }else{
+                    int insertOp = dp[i][j-1];
+                    int deleteOp = dp[i-1][j];
+                    int replaceOp = dp[i-1][j-1];
+                    dp[i][j] = 1+min({insertOp,deleteOp,replaceOp});
+                }
+            }
+        }
+
+        return dp[l1][l2];
     }
 };
