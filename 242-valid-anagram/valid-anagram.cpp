fccd0cb189604bfd7afe2273ac3f180cfd05ccf5
divyanshu-dev <divyanshushukla1947@gmail.com>
2026-04-08 22:12:45 +0530

    Time: 1 ms (54.79%) | Memory: 10 MB (10.41%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,32 +1,23 @@
-#include <string>
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-       unordered_map<char,int> mp;
-
-       if(s.length()!=t.length()){
-        return false;
-       }
-       for(char ch: s){
-            if(!mp.count(ch)){
-                mp[ch] = 1;
-                continue;
-            }
-            mp[ch]++;
-       }
-       for(char ch: t){
-            if(!mp.count(ch)){
-                return false;
-            }
-            mp[ch]--;
-       }
-       
-       for(pair<char,int>p:mp){
-        if(p.second){
+        unordered_map <int,int> mp;
+        if(s.length()!=t.length()){
             return false;
         }
-       }
-       return true;
+        for(char ch: s){
+            mp[ch]++;
+        }
 
+        for(char ch: t){
+            mp[ch]--;
+        }
+
+        for(auto &p: mp){
+            if(p.second>0){
+                return false;
+            }
+        }
+        return true;
     }
 };
