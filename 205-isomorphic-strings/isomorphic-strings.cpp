5c52c4248f5e00f3a6d97fbec8fafa645e355a0a
divyanshu-dev <divyanshushukla1947@gmail.com>
2026-04-09 11:18:51 +0530

    Time: 3 ms (39.82%) | Memory: 9.5 MB (27.37%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -4,23 +4,18 @@ public:
         if(s.length()!=t.length()) return false;
 
         unordered_map<char,char> mp;
+        unordered_map<char,char> map;
 
         for(int i=0; i<s.length(); i++){
             if(mp.count(s[i])!=0 && mp[s[i]]!=t[i]){
                 return false;
             }
-            
-            mp[s[i]] = t[i];
-        }
-
-        unordered_map<char,char> map;
-
-        for(int i=0; i<s.length(); i++){
             if(map.count(t[i])!=0 && map[t[i]]!=s[i]){
                 return false;
             }
             
             map[t[i]] = s[i];
+            mp[s[i]] = t[i];
         }
 
         return true;
