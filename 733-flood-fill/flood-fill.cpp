ab24c7c6633b19e6b5c27abbfebac5d66bcf1337
divyanshu-dev <divyanshushukla1947@gmail.com>
2026-04-11 12:01:47 +0530

    Time: 0 ms (100.00%) | Memory: 18.2 MB (56.01%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
-    void dfs(int r, int c, int st, int color, vector<vector<bool>> &visited,vector<vector<int>>& image){
-        if(r<0 || c<0 || r>=image.size() || c>=image[0].size() || image[r][c]!=st || visited[r][c]) return;
+    void dfs(vector<vector<int>>& image,int r,int st, int c, int color,vector<vector<bool>> &vis){
+        if(r<0 || c<0 || r>=image.size() || c>=image[0].size() || image[r][c]!=st || vis[r][c]){
+            return;
+        }
 
-        visited[r][c] = true;
         image[r][c] = color;
+        vis[r][c] = true;
 
-        dfs(r,c+1,st,color,visited,image);
-        dfs(r,c-1,st,color,visited,image);
-        dfs(r+1,c,st,color,visited,image);
-        dfs(r-1,c,st,color,visited,image);
+        dfs(image,r+1,st,c,color,vis);
+        dfs(image,r,st,c+1,color,vis);
+        dfs(image,r,st,c-1,color,vis);
+        dfs(image,r-1,st,c,color,vis);
+
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int m = image.size(); 
-        int n = image[0].size();
-
-        int st = image[sr][sc];
-
-        vector<vector<bool>> visited(m,vector<bool>(n,false));
-
-        dfs(sr,sc,st,color,visited,image);
-
+        int n = image.size();
+        int m = image[0].size();
+        vector<vector<bool>> vis(n, vector<bool>(m, false));
+        dfs(image,sr,image[sr][sc],sc,color,vis);
         return image;
     }
 };
