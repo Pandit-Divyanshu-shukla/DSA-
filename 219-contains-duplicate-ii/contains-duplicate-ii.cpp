e84c8784ca596fd3b13149a650c48ca5ae096aeb
divyanshu-dev <divyanshushukla1947@gmail.com>
2026-04-14 11:05:05 +0530

    Time: 74 ms (62.31%) | Memory: 98.6 MB (46.22%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.cpp
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -4,13 +4,12 @@ public:
         unordered_map<int,int> mp;
 
         for(int i=0; i<nums.size(); i++){
-            if(mp.count(nums[i])){
-                if(i-mp[nums[i]]<=k){
-                    return true;
-                }
-            }
+            if(mp.count(nums[i]) && abs(i-mp[nums[i]])<=k) return true;
+
             mp[nums[i]] = i;
         }
+        
+
 
         return false;
     }
