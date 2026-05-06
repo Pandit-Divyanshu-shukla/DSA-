b04cfd37addc3808572d66648c402ee1b6d36108
divyanshu-dev <divyanshushukla1947@gmail.com>
2026-05-06 09:29:39 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Check K-th Bit/check-kth-bit.c
+++ Difficulty: Easy/Check K-th Bit/check-kth-bit.c
@@ -1,6 +1,8 @@
 #include <stdbool.h>
 int checkKthBit(int n, int k) {
     // code here
-    if((1<<k)&n) return true;
-    return false;
+    int bitMask  = 1<<k;
+    if(n&bitMask) return 1;
+    return 0;
+    
 }
