7bf12109b69108b3431626f909c071c8999a7bec
divyanshu-dev <divyanshushukla1947@gmail.com>
2026-05-10 10:50:42 +0530

    Time: 84 ms (35.31%) | Memory: 176.6 MB (9.71%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,42 +1,52 @@
 class Solution {
 public:
-    int solve(int st, int end, string &s,vector<vector<int>> &dp){
-        if(st>end){
-            return 1;
+    // bool chkPalindrome(string s, int st, int end){
+
+    //     while(st<end){
+    //         if(s[st++]!=s[end--]){
+    //             return false;
+    //         }
+    //     }
+
+    //     return true;
+    // }
+
+    void chkExpansion(string s, int l, int r, string &ans){
+        
+
+        while(l>=0 && r<s.length() && s[l]==s[r]){
+            if((r-l+1) > ans.length()){
+                ans = s.substr(l,r-l+1);
+            }
+            l--;
+            r++;
         }
-
-        if(dp[st][end]!=-1){
-            return dp[st][end];
-        }
-
-        if(s[st]==s[end]){
-            return dp[st][end]=solve(st+1,end-1,s,dp);
-        }else{
-            return dp[st][end]=0;
-        }
-
-        return 1;
-
     }
+    // void generateSubstrings(string s, string &ans){
+    //     for(int i=0; i<s.length(); i++){
+    //         char ch = s[i];
+    //         string sub ="";
+    //         sub+=ch;
+    //         if(chkPalindrome(sub) && ans.length()<sub.length()){
+    //             ans = sub;
+    //         }
+    //         for(int j=i+1; j<s.length(); j++){
+    //             sub+=s[j];
+    //             if(chkPalindrome(sub) && ans.length()<sub.length()){
+    //                 ans = sub;
+    //             }
+    //         }
+    //     }
+    // }
     string longestPalindrome(string s) {
-
-        int count=0;
-        int maxLen = 0;
-        string maxSub="";
-
-        vector<vector<int>> dp(s.length()+1,vector<int>(s.length()+1,-1));
+        string ans = "";
 
         for(int i=0; i<s.length(); i++){
-            for(int j=i; j<s.length(); j++){
-                if(solve(i,j,s,dp)){
-                    int len = j-i+1;
-                    if(len>maxLen){
-                        maxSub = s.substr(i,len);
-                        maxLen = max(j-i+1,maxLen);
-                    }
-                }
-            }
+            // even expansion
+            chkExpansion(s,i,i+1,ans);
+            // odd expansion
+            chkExpansion(s,i,i,ans);
         }
-        return maxSub;
+        return ans;
     }
 };
