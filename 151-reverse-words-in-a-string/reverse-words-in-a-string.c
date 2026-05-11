bc9c48614d3715591b1c08d37a180425205a1809
divyanshu-dev <divyanshushukla1947@gmail.com>
2026-05-11 21:06:47 +0530

    Time: 1 ms (25.27%) | Memory: 9.3 MB (54.21%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.c
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.c
@@ -1,53 +1,41 @@
-void reverse(char *s, int left, int right){    
-    while(left<right){
-        char temp = s[left];
-        s[left++] = s[right];
-        s[right--] = temp;
+void reverse(char *s, int l, int r){
+    while(l<r){
+        int temp = s[l];
+        s[l++] = s[r];
+        s[r--] = temp;
     }
-
 }
 
-void removeSpaces(char *s, char*rev){
-    int n  =strlen(s);
-    int i=0;
+void removeSpaces(char*s, char *rev){
+    int i =0;
     int idx=0;
-
-    while(i<n){
+    int n = strlen(s);
+    while(i<strlen(s)){
         while(i<n && s[i]==' ') i++;
-
-        while(i<n && s[i]!=' '){
-            rev[idx++] = s[i++];
-        }
-
-        while(i<n && s[i]==' '){
-            i++;
-        }
-
-        if(i<n){
-            rev[idx++] = ' ';
-        }
+        while(i<n && s[i]!=' ') rev[idx++] = s[i++];
+        while(i<n && s[i]==' ') i++;
+        if(i<strlen(s)) rev[idx++] = ' ';
     }
-
-    rev[idx] = '\0';
+    rev[idx]='\0';
 }
+
 char* reverseWords(char* s) {
     int n = strlen(s);
-    char * rev= (char*) malloc((n+1)*sizeof(char));
+    int st=0;
+
+    char* rev = (char*) malloc((n+1));
+    int idx = 0;
 
     reverse(s,0,n-1);
 
-    int st = 0;
-
-    for(int i=0; i<=strlen(s); i++){
+    for(int i=0; i<=n; i++){
         if(s[i]==' ' || s[i]=='\0'){
-            reverse(s,st,i-1);
-            // s[i] = ' '; 
-            st = i+1;
+           reverse(s,st,i-1);
+           st = i+1;
         }
     }
 
     removeSpaces(s,rev);
 
     return rev;
-    
 }
